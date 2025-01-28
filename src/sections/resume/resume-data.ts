78e3df193edf695534d0ed8c8b58a8f8013346f1
AceGjorgjievski <ace.gjorgjievski6@gmail.com>
2025-01-28 10:30:58 +0100

    feat: setup the resume page

---
+++ src/sections/resume/resume-data.ts
@@ -0,0 +1,120 @@
+
+
+export const experience = {
+    icon: "",
+    title: "My Experience",
+    description: "Lorem Ipsum",
+    items: [
+        {
+            company: "MVP Masters",
+            position: "Junior Software Engineer - Internship",
+            diration: "Oct 2024 - Dec 2024",
+            responsibility: "Developing new features and ongoing improvements for the trgo24.mk website using clean & maintanable code by minimals.cc"
+        },
+        {
+            company: "DDCOM",
+            position: "IT Support",
+            duration: "Aug 2019",
+            responsibility: "Installing Windows Operating Systems"
+        }
+    ]
+};
+
+export const education = {
+    icon: "",
+    title: "My Education",
+    description: "Lorem Ipsum",
+    items: [
+        {
+            institution: "Faculty of Computer Science and Engineering, Skopje",
+            major: "Software Engineering and Information Systems",
+            duration: "2020 - 2025",
+            degree: "Bachelor's Degree (josh malo)"
+        },
+        {
+            institution: "SUGS 'VLADO TASEVSKI', Skopje",
+            major: "Electrical technician for Electrical Engineering and Telecommunications",
+            duration: "2016 - 2020",
+            degree: "Diploma in Electrical Engineering and Telecommunications"
+        },
+    ]
+};
+
+export const skills = {
+    title: "My Skills",
+    description: "",
+    skillList: [
+        {
+            icon: '',
+            name: "Java"
+        },
+        {
+            icon: '',
+            name: "Spring Boot"
+        },
+        {
+            icon: '',
+            name: "C#"
+        },
+        {
+            icon: '',
+            name: "ASP.NET Core"
+        },
+        {
+            icon: '',
+            name: "React js"
+        },
+        {
+            icon: '',
+            name: "Next js"
+        },
+        {
+            icon: '',
+            name: "Firebase"
+        },
+        {
+            icon: '',
+            name: "Nest js"
+        },
+        {
+            icon: '',
+            name: "Flutter"
+        },
+        {
+            icon: '',
+            name: "Git"
+        },
+        {
+            icond: '',
+            name: "Sony Vegas Pro"
+        }
+    ]
+};
+
+export const aboutMe = {
+    title: "About Me",
+    description: "Lorem Ispum",
+    info: [
+        {
+            fieldName: "Email",
+            fieldValue: "ace.gjorgjievski6@gmail.com"
+        },
+        {
+            fieldName: "Experience",
+            fieldValue: "2+ Years"
+        },
+        {
+            fieldName: "Country",
+            fieldValue: "Macedonia"
+        },
+        {
+            fieldName: "City",
+            fieldValue: "Skopje"
+        },
+        {
+            fieldName: "Language",
+            fieldValue: "English, Macedonian"
+        }
+    ]
+};
+
--- src/sections/resume/view.tsx
+++ src/sections/resume/view.tsx
@@ -1,8 +1,97 @@
+'use client';
+
+import { useResponsive } from "@/hooks/use-response";
+import { Container, Stack } from "@mui/material";
 
 export default function ResumeView() {
+
+    const isSmUp = useResponsive("up", "sm");
+    const isMdUp = useResponsive("up", "md");
+
     return (
-        <div>
-            Resume View
-        </div>
+        <>
+            <Container
+                sx={{
+                    marginTop: '100px'
+                }}
+            >
+                {
+                    (isSmUp) ? (
+                        <>
+                            <Stack
+                                direction="row"
+                                spacing={2}
+                                justifyContent="center"
+                                alignItems="center"
+                            >
+                                <Container
+                                    sx={{
+                                        display: 'flex',
+                                        flexDirection: 'row',
+                                        justifyContent: 'space-between',
+                                        alignItems: 'flex-start',
+                                        gap: '20px',
+                                    }}
+                                >
+                                    <Container
+                                        sx={{
+                                            flex: '1',
+                                            marginTop: (isMdUp ? '80px' : isSmUp ? '0px' : ''),
+                                        }}
+                                    >
+
+                                        123 - w
+                                    </Container>
+                                    <Container
+                                        sx={{
+                                            flex: '1',
+                                            display: 'flex',
+                                            justifyContent: 'center',
+                                            alignItems: 'center',
+                                        }}
+                                    >
+                                        456 - w
+                                    </Container>
+
+                                </Container>
+                            </Stack>
+                        </>
+                    ) : (
+                        <>
+                            <Stack
+                                direction="column"
+                                spacing={2}
+                                justifyContent="center"
+                                alignItems="center"
+                            >
+                                <Container>
+                                    <Container
+                                        sx={{
+                                            textAlign: 'center',
+                                            padding: '20px',
+                                            borderRadius: '8px',
+                                            color: 'white',
+                                            marginBottom: '40px'
+                                        }}
+                                    >
+                                        123 - m
+                                    </Container>
+                                    <Container
+                                        sx={{
+                                            padding: '20px',
+                                            borderRadius: '8px',
+                                            color: 'white',
+                                        }}
+                                    >
+                                        456 - m
+                                    </Container>
+                                </Container>
+
+                            </Stack>
+                        </>
+                    )
+                }
+            </Container>
+        </>
     );
 }
